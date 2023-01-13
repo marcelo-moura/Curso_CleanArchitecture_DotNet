bc1db87ab9561784312dd75d88734c68f64bb799
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2023-01-13 19:55:42 -0300

    Implementação do padrão Mediator e CQRS, criação dos Handlers e classes de Command e Query

---
+++ CleanArchMvc/CleanArchMvc.Application/Products/Commands/ProductDeleteCommand.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Commands
+{
+    public class ProducDeleteCommand : IRequest<Product>
+    {
+        public int? Id { get; set; }
+
+        public ProducDeleteCommand(int? id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Application/Products/Handlers/ProductDeleteCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class ProductDeleteCommandHandler : IRequestHandler<ProducDeleteCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductDeleteCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(ProducDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product is null)
+            {
+                throw new ApplicationException($"Entity could not be found.");
+            }
+            else
+            {
+                await _productRepository.DeleteAsync(request.Id);
+                return product;
+            }
+        }
+    }
+}
