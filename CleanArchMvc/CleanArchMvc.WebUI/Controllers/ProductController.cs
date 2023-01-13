cb56cedaec716adc6962f7a42b47beeb658c682a
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2023-01-12 21:54:54 -0300

    Implmentação das Controllers na camada de apresentação e criação das primeiras Views

---
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products);
+        }
+    }
+}
