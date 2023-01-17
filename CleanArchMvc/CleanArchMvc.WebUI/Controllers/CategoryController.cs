3c00cd09a89eab1d480529ecf5bd5b9bdbb530bb
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2023-01-17 19:28:47 -0300

    Implementação para criar uma Categoria

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/CategoryController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -12,10 +13,29 @@ namespace CleanArchMvc.WebUI.Controllers
             _categoryService = categoryService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var categories = await _categoryService.GetAllAsync();
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.CreateAsync(categoryDTO);
+                return RedirectToAction("Index");
+            }
+
+            return View(categoryDTO);
         }
     }
 }
