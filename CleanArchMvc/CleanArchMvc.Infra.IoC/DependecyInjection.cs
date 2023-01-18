d1d1759f0f256cc3ecfa375fc1a5626c49f2f7c5
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2023-01-18 19:51:39 -0300

    Registro das injeções de dependencia para o Identity e configuração de cookie

--- CleanArchMvc/CleanArchMvc.Infra.IoC/DependecyInjection.cs
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependecyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using CleanArchMvc.Application.AutoMappings;
 using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repository;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,12 +22,22 @@ namespace CleanArchMvc.Infra.IoC
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                     x => x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+                options.AccessDeniedPath = "/Account/Login");
+
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<IProductServiceComMediator, ProductServiceComMediator>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile), typeof(DTOToCommandMappingProfile));
 
